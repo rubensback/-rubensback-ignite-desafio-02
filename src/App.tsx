31a150dbb94b9121b18fb427fa5f446e32c96a4d
rubensback <rubensback@gmail.com>
2022-11-01 18:41:55 +0000

    init project and base config

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <div>Hello world</div>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
